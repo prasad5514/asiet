179865fb7ddcb3b64dbc86ef5c781c897c7d9ca4
prasad5514 <prasadeee41@gmail.com>
2020-03-10 05:12:44 +0530

    till while loop

---
+++ check.js
@@ -0,0 +1,7 @@
+var x=13;
+if(x%2==0){
+    console.log("even number");
+}
+else{
+    console.log("odd number");
+}
---
+++ const.js
@@ -0,0 +1,3 @@
+const x=14;
+x=x+10;
+console.log(x);//only one time value cangiven to the const date type
